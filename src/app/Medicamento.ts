b1225e7deab1054a7fa224d7ffc2cd324184937a
Ruby020905 <rubyoforiwa@gmail.com>
2026-01-14 22:36:40 -0500

    initial commit

---
+++ src/app/Medicamento.ts
@@ -0,0 +1,11 @@
+export interface Medicamento {
+  id?: number;
+  nombre: string;
+  tipo: string;
+  presentacion: string;
+  unidad: string;
+  cantidad: number;
+  fechaIngreso: string;
+  fechaSalida: string;
+  fechaCaducidad: string;
+}
